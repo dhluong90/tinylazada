debd9ed2993ee8398e1464ee2ee9d34cb2e9edcf
dhluong <dhluong90@gmail.com>
2021-09-29 22:00:59 +0800

    adding microservice template

---
+++ backend/identity-server/src/test/java/com/luongdinh/identityserver/IdentityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luongdinh.identityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
